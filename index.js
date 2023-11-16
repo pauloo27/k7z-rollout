a6289e73865f969d5260c7974f6b62f357d981fe
Paulo <git@pauloo27.dev>
2023-11-16 15:53:53 -0300

    feat: update k7z restart url

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-const core = require('@actions/core');
+const core = require("@actions/core");
 //const core = require('./mocked-core');
-const https = require('node:https');
+const https = require("node:https");
 
 try {
-  const projectId = core.getInput('project_id');
-  const secret = core.getInput('secret');
-  const secretHeader = core.getInput('secret_header');
-  const baseURL = core.getInput('base_url');
+  const projectId = core.getInput("project_id");
+  const secret = core.getInput("secret");
+  const secretHeader = core.getInput("secret_header");
+  const baseURL = core.getInput("base_url");
 
-  const url = new URL(`${baseURL}/projects/${projectId}/reload`);
+  const url = new URL(`${baseURL}/projects/${projectId}/restart`);
 
   core.info(`Reloading project ${projectId} at ${url}`);
 
   const req = https.request(
     {
-      method: 'POST',
+      method: "POST",
       hostname: url.hostname,
       path: url.pathname,
       headers: {
         [secretHeader]: secret,
       },
-    }, 
+    },
 
     (res) => {
       const statusCode = res.statusCode;
       if (statusCode === 200) {
-        core.setOutput('status', statusCode);
+        core.setOutput("status", statusCode);
       } else {
         core.setFailed(`Request failed with status code ${statusCode}`);
       }
     },
   );
 
-  req.on(
-    'error', 
-    (error) => { 
-      core.setFailed(error.message);
-    },
-  )
+  req.on("error", (error) => {
+    core.setFailed(error.message);
+  });
 
   req.end();
 } catch (error) {
